a5b8965bcdd86b5066963565e0c7c970dafa34e5
spandana.gathram <gathramspandana93@gmail.com>
2016-04-25 18:31:27 +0530

    Interbus changes

---
+++ INTERBUSWebsite/Controllers/TicketBookingController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace KESENENI2.Controllers
+{
+    public class KESENINE3Controller : ApiController
+    {
+          [HttpGet]
+
+        public DataTable commericialsite()
+        {
+            DataTable Tbl = new DataTable();
+
+
+            //connect to database
+            SqlConnection conn = new SqlConnection();
+            //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            conn.ConnectionString = "Data Source=localhost;Initial Catalog=INTERBUS;Integrated Security=SSPI;";
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "Gettable2";
+            cmd.Connection = conn;
+          
+            DataSet ds = new DataSet();
+            SqlDataAdapter db = new SqlDataAdapter(cmd);
+            db.Fill(ds);
+            Tbl = ds.Tables[0];
+
+            // int found = 0;
+            return Tbl;
+
+        }
+
+    }
+}
+
+  
---
+++ INTERBUSWebsite/UI/js/LicencePage1.js
@@ -0,0 +1,47 @@
+﻿// JavaScript source code
+var app = angular.module('myApp', [])
+var ctrl = app.controller('myCtrl', function ($scope, $http) {
+
+    $scope.GoToConfirmation = function (code) {
+
+        if (code == null) {
+            alert('please enter valid fleet owner code or contact administrator.');
+            return false;
+        }
+        else {
+            $http.get('http://localhost:1476/api/fleetownerlicense/validatefleetowner?fleetownercode=' + code).then(function (response, req) {
+                $scope.result = response.data;
+
+                if ($scope.result > 0)
+                    window.location.href = "http://localhost:1476/CommercialSite/LicenseConfirmation.html";
+                else
+                    alert('invalid fleet owner code');
+
+            });
+        }   
+    };
+
+    $scope.GoTobuy = function (code, units) {
+
+        if (code == null) {
+            alert('please enter valid fleet owner code or contact INTERBUS administrator.');
+            return false;
+        } else {
+            if (units == null) {
+                alert('please enter valid no. of units.');
+                return false;
+            }
+            else {
+                $http.get('http://localhost:1476/api/fleetownerlicense/updatebtpos?fleetownercode=' + code+ '&units='+units).then(function (response, req) {
+                    $scope.result = response.data;
+
+                    if ($scope.result == 0)
+                        alert('invalid fleet owner code or BT POS units not available. Please contact INTERBUS admin');
+                    else
+                        alert('Please login into BT POS dashboard and activate the BT POS ' + units + ' unit(s)');
+                    
+                });
+            }
+        }
+    };
+});
