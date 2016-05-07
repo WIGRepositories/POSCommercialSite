feb79c6f5b64b57cb1693e2d18d51e9a0f3cd400
spandana.gathram <gathramspandana93@gmail.com>
2016-05-07 16:50:06 +0530

    Booking,indexchanges

--- INTERBUSWebsite/UI/js/Index.js
+++ INTERBUSWebsite/UI/js/Index.js
@@ -1,3 +1,31 @@
+
+var app = angular.module('myApp', [])
+var ctrl = app.controller('myCtrl', function ($scope, $http) {
+    $http.get('http://localhost:52800/api/Stops/commericialsite').then(function (response, req) {
+        $scope.Stops = response.data;
+
+        
+    })
+
+    $scope.routes = [{
+        Tn: 'Star Travels',
+        Time: '0:00 PM -05:45 AM',
+        Seats: '9 Seats',
+        Price: '500'
+
+    },
+    {
+        Tn:'OVR Travels',
+        Time:'0:00 PM -05:45 AM',
+        Seats:'30 Seats',
+        Price: '400'
+    
+        
+    }];
+});
+    
+
+
 function fun() {
     if (document.getElementById("ddlBusType").value == 2) {//if Booking Type
         window.location.href = "booking.html";
