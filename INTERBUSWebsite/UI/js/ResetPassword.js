b74a756b30ebd3396715bf4fdafda8099d08278c
pmunipavani <pmunipavani@gmail.com>
2016-06-07 16:47:12 +0530

    interbuslogin completed

---
+++ INTERBUSWebsite/UI/js/ResetPassword.js
@@ -0,0 +1,30 @@
+﻿var myapp1 = angular.module('myApp', [])
+var mycrtl1 = myapp1.controller('Mycntrl', function ($scope, $http) {
+
+   
+    $scope.save = function (type) {
+
+        var type = {
+
+            UserName: type.UserName,
+            oldPassword: type.oldPassword,
+            newPassword: type.newPassword,
+            reenternewPassword: type.reenternewPassword,
+           
+        };
+
+        var req = {
+            method: 'POST',
+            url: ('http://localhost:1476/api/RegisterForm/pos'),
+            //headers: {
+            //    'Content-Type': undefined
+
+            data: type
+
+
+        }
+        $http(req).then(function (response) { });
+
+    }
+
+});
