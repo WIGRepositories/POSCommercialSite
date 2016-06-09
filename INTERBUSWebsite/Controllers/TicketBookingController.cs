e78ba1b2d0b60d2e4e45a21bbc7c737fb2930133
srujansimha <msrujansimha@gmail.com>
2016-06-09 19:58:14 +0530

    Ticket Booking 6/9

--- INTERBUSWebsite/Controllers/TicketBookingController.cs
+++ INTERBUSWebsite/Controllers/TicketBookingController.cs
@@ -66,8 +66,34 @@ namespace INTERBUSWebsite.Controllers
             int a = Tbl.Rows.Count;
 
                 return Tbl;
-       
-        }
+          }
+
+//[HttpGet]
+//          public DataTable GetTicketDetails(int srcId, int destId)
+//          {
+//              DataTable Tbl = new DataTable();
+
+
+//              //connect to database
+//              SqlConnection conn = new SqlConnection();
+//              //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+//              conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+
+//              SqlCommand cmd = new SqlCommand();
+//              cmd.CommandType = CommandType.StoredProcedure;
+//              cmd.CommandText = "Getsp_Availableseats";
+//              cmd.Parameters.Add("Src_Id", SqlDbType.Int).Value = srcId;
+//              cmd.Parameters.Add("Des_Id", SqlDbType.Int).Value = destId;
+//              cmd.Connection = conn;
+
+//              DataSet ds = new DataSet();
+//              SqlDataAdapter db = new SqlDataAdapter(cmd);
+//              db.Fill(ds);
+//              Tbl = ds.Tables[0];
+//              int a = Tbl.Rows.Count;
+
+//              return Tbl;
+//          }
 
     [HttpPost]
           public DataTable SaveBookingDetails(Booking B)
