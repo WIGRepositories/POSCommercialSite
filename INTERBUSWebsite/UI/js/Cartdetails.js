7d61eabf5b056f06290615b823281af1abe1dfaa
NaveenKumarBeesu <naveenkumarbeesu@gmail.com>
2016-06-23 17:38:54 +0530

    commit

--- INTERBUSWebsite/UI/js/Cartdetails.js
+++ INTERBUSWebsite/UI/js/Cartdetails.js
@@ -4,11 +4,22 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
     $scope.FillDetails = function () {
         $scope.FillDetails = function () {
             $scope.licenseCatId = $localStorage.LicenseTypeId;
-            $scope.fodetails = $localStorage.FleetOwner;
+            $scope.fodetails = $localStorage.FleetOwnerCode;
 
             var ldetails = $localStorage.License;
             $scope.selLicense = "";
             $scope.selLicensePrice = "";
+   
+          
+            //displaying the current fleet owner code
+            var fodetails = $localStorage.FleetOwnerCode;
+            $scope.selFleetOwnerCode = "";
+         
+            if (FleetOwnerCode != null)
+            {
+                $scope.selFleetOwnerCode = fodetails.FleetOwnerCode;
+                }
+             
 
             //identify the selected license and display the properties
             for (ltCnt = 0; ltCnt < ldetails.Table.length; ltCnt++) {
@@ -21,37 +32,36 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
             for (ltCnt = 0 ; ltCnt < ldetails.Table2.length; ltCnt++) {
                 if (ldetails.Table2[ltCnt].LicenseId == $scope.selLicense.Id) {
                     $scope.selLicensePrice = ldetails.Table2[ltCnt]
-            }
-        }
-    }
-
-    $scope.processPymt = function () {
-        //save the details into db for user license
-        if(months < 0)
-        {
-            alert("Invalid data");
-        }
-        else{
-            $scope.Licensepayments = function () {
-
-                var pmnt = {
-                    LicenseTypeId: LicenseTypeId,
-                    fleetownercode:code 
-                };
-                $localstorage.value = pmnt;
-                var req = {
-                    method: 'POST',
-                    url: 'http://localhost:52800/api/LicensePage/SaveLicence',
-                    data: pmnt
                 }
-                $http(req).then(function (response) {
-                    alert(response.data);
+            }
 
         }
-        //create a user login for the fleet owner
-        //communicate the same to user
 
-        window.location.href = "http://localhost:52800/UI/LicenseConfirmation.html";
+        $scope.processPymt = function () {
+            //save the details into db for user license
+            // if(No. of Months < 0)
+             //  {
+             //      alert("Invalid data");
+            //   }
+             //   else{
+            //        $scope.Licensepayments = function () {
+
+            //            var pmnt = {
+            //                LicenseTypeId: LicenseTypeId,
+            //                fleetownercode:code 
+            //            };
+            //           
+            //            }
+            //            $http(req).then(function (response) {
+            //                alert(response.data);
+
+            //            }
+            //create a user login for the fleet owner
+            //communicate the same to user
+
+            window.location.href = "http://localhost:52800/UI/LicenseConfirmation.html";
+
+        }
     }
-
-});
+            });
+        
