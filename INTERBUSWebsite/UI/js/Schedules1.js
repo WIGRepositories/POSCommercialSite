63be222e0e945a46a14197135bff569af4811dc4
NaveenKumarBeesu <naveenkumarbeesu@gmail.com>
2016-07-28 18:44:29 +0530

    get schedules

---
+++ INTERBUSWebsite/UI/js/Schedules1.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('myApp', [])
+var ctrl = app.controller('myCtrl', function ($scope, $http) {
+   
+    $scope.GetSchedules = function () {
+
+        $http.get('http://localhost:52800/api/Schedules/GetSchedules').then(function (response, req) {
+            $scope.Schedules = response.data;
+        });
+    }
+
+});
