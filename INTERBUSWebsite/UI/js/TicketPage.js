f78e9b0ebee02e25c1a8919c0fc42ced876f2710
srujansimha <msrujansimha@gmail.com>
2016-12-03 22:20:53 +0530

    Ticket format and printing

--- INTERBUSWebsite/UI/js/TicketPage.js
+++ INTERBUSWebsite/UI/js/TicketPage.js
@@ -5,12 +5,19 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
    
 
     $scope.GetDetails = function () {
-        $scope.source = $localStorage.srcId;
-        $scope.destination = $localStorage.destId;
-        $scope.details = $localStorage.book;
-        $scope.firstName = $scope.details.No_Seats;
-        $scope.NoofSeats = $scope.details.NoofSeats;
-        $scope.JourneyType = $scope.details.JourneyType;
+
+        //get the booking id and get the details from db
+        //if obtained from db then take it otherwise retive from session
+
+        //$scope.onwarddetails =  $localStorage.onwarddetails;
+
+        //$scope.source = $localStorage.srcId;
+        //$scope.destination = $localStorage.destId;
+        //$scope.details = $localStorage.book;
+        //$scope.firstName = $scope.details.No_Seats;
+        //$scope.NoofSeats = $scope.details.NoofSeats;
+        //$scope.JourneyType = $scope.details.JourneyType;
+
 
 
         //$http.get('/api/TicketBooking/GetCities?srcId=' + $scope.source + '&destId=' + $scope.destination).then(function (response, req) {
@@ -18,6 +25,19 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage) {
         //    $scope.From = $scope.services.source;
         //    $scope.To = $scope.services.destination;
         //})
+    }
+
+    $scope.printDiv = function (div) {
+        var docHead = document.head.outerHTML;
+        var printContents = document.getElementById(div).outerHTML;
+        var winAttr = "location=yes, statusbar=no, menubar=no, titlebar=no, toolbar=no,dependent=no, width=865, height=600, resizable=yes, screenX=200, screenY=200, personalbar=no, scrollbars=yes";
+
+        var newWin = window.open("", "_blank", winAttr);
+        var writeDoc = newWin.document;
+        writeDoc.open();
+        writeDoc.write('<!doctype html><html>' + docHead + '<body onLoad="window.print()">' + printContents + '</body></html>');
+        writeDoc.close();
+        newWin.focus();
     }
 
    
